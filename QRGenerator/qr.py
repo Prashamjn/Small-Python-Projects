3cb1b00e6e1f45e084a76c6bcf29dcdc760362a9
Prasham Jain <67408193+Prashamjn@users.noreply.github.com>
2026-08-28 11:41:06 +0530

    Enhance QR code generation with output folder
    Added functionality to create an output folder for generated QR codes and save the QR code image in that folder.

--- QRGenerator/qr.py
+++ QRGenerator/qr.py
@@ -1,11 +1,21 @@
 import qrcode
+import os
 
-data = input("ENTER TEXT OR LINK :")
+# Create output folder
+output_folder = "Generated QR Codes"
+os.makedirs(output_folder, exist_ok=True)
 
+# Get data from user
+data = input("ENTER TEXT OR LINK: ")
+
+# Generate QR code
 img = qrcode.make(data)
 
-img.save("img.png")
+# Save QR code
+output_path = os.path.join(output_folder, "img.png")
+img.save(output_path)
 
+# Open generated QR code
 img.show()
 
-print("QRCODE GENRATED !!!")
+print(f"QRCODE GENERATED AND SAVED IN '{output_folder}/img.png' !!!")
